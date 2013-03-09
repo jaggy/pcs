fd00c6b1c4defc9624ee7bf73e26e6f8ff7eeb9c
Jaggy Gauran <jaggygauran@gmail.com>
2013-03-10 07:57:40 +0800

    created roles and related it to the user

--- Model/User.php
+++ Model/User.php
@@ -3,4 +3,11 @@
 App::uses('AppModel', 'Model');
 class User extends AppModel {
 
+  public $belongsTo = array(
+    'Role' => array(
+      'className' => 'Role',
+      'foreignKey' => 'role_id'
+    )
+  );
+
 }
