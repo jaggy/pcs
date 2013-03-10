ab06211485537a65823d2e0f58b480cd5ddc09fa
Jaggy Gauran <jaggygauran@gmail.com>
2013-03-11 02:36:12 +0800

    removed the default routes

--- Config/routes.php
+++ Config/routes.php
@@ -3,9 +3,10 @@
 	Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
 	Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
 
+  Router::connect('/register', array('controller' => 'users', 'action' => 'register')); 
   Router::connect('/activate/*', array('controller' => 'users', 'action' => 'activate'));
   
 	CakePlugin::routes();
 
   // CakePHP default routes
-	require CAKE . 'Config' . DS . 'routes.php';
+	// require CAKE . 'Config' . DS . 'routes.php';
