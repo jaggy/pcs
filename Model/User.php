ff9dfe6ae91f9c5efe57b515df8ab2a6a2227bb1
Jaggy Gauran <jaggygauran@gmail.com>
2013-03-11 02:08:24 +0800

    cakephp basic validation

--- Model/User.php
+++ Model/User.php
@@ -4,6 +4,83 @@ App::uses('AppModel', 'Model');
 App::uses('AuthComponent', 'Controller/Component');
 class User extends AppModel {
 
+  public $validate = array(
+    'first_name' => array(
+      'notempty'   => array(
+        'rule'    => array('notempty'),
+        'message' => 'first name cannot be empty',
+        //'allowEmpty' => false,
+        //'required'   => false,
+        //'last'       => fa;se, // Stop validation after this rule
+        //'on'         => 'create' // Limit validation to 'create' or 'update' operations
+      ),
+    ),
+    'last_name' => array(
+      'notempty'   => array(
+        'rule'    => array('notempty'),
+        'message' => 'last name cannot be empty',
+        //'allowEmpty' => false,
+        //'required'   => false,
+        //'last'       => fa;se, // Stop validation after this rule
+        //'on'         => 'create' // Limit validation to 'create' or 'update' operations
+      ),
+    ),
+    'username' => array(
+      'isUnique'   => array(
+        'rule'    => array('isUnique'),
+        'message' => 'username already taken',
+        //'allowEmpty' => false,
+        //'required'   => false,
+        //'last'       => fa;se, // Stop validation after this rule
+        //'on'         => 'create' // Limit validation to 'create' or 'update' operations
+      ),
+      'notempty'   => array(
+        'rule'    => array('notempty'),
+        'message' => 'username cannot be blank',
+        //'allowEmpty' => false,
+        //'required'   => false,
+        //'last'       => fa;se, // Stop validation after this rule
+        //'on'         => 'create' // Limit validation to 'create' or 'update' operations
+      ),
+      'between'   => array(
+        'rule'    => array('between', 5, 18),
+        'message' => 'username must be between 5 and 18 characters',
+        //'allowEmpty' => false,
+        //'required'   => false,
+        //'last'       => fa;se, // Stop validation after this rule
+        //'on'         => 'create' // Limit validation to 'create' or 'update' operations
+      ),
+    ),
+    'password' => array(
+      'notempty'   => array(
+        'rule'    => array('notempty'),
+        'message' => 'password cannot be empty',
+        //'allowEmpty' => false,
+        //'required'   => false,
+        //'last'       => fa;se, // Stop validation after this rule
+        //'on'         => 'create' // Limit validation to 'create' or 'update' operations
+      ),
+      'minLength'   => array(
+        'rule'    => array('minLength', 6),
+        'message' => 'password cannot be less than 6 characters',
+        //'allowEmpty' => false,
+        //'required'   => false,
+        //'last'       => fa;se, // Stop validation after this rule
+        //'on'         => 'create' // Limit validation to 'create' or 'update' operations
+      ),
+    ),
+    'email' => array(
+      'email'   => array(
+        'rule'    => array('email'),
+        'message' => 'email not valid',
+        //'allowEmpty' => false,
+        //'required'   => false,
+        //'last'       => fa;se, // Stop validation after this rule
+        //'on'         => 'create' // Limit validation to 'create' or 'update' operations
+      ),
+    )
+  );
+
 
   /**
    * Add the default membership status if it is not existing
