b4cacbbd63fc4cb7facc61bce583fbcb3d3372c5
Jaggy Gauran <jaggygauran@gmail.com>
2013-03-10 08:16:11 +0800

    register the user as a reguar member on register

--- Model/User.php
+++ Model/User.php
@@ -3,6 +3,27 @@
 App::uses('AppModel', 'Model');
 class User extends AppModel {
 
+
+  public function beforeSave($options = array()){
+    parent::beforeSave($options);
+
+    if(!isset($this->data['User']['role_id'])){
+      $this->Role->Behaviors->attach('Containable');
+      $role = $this->Role->find('first', array(
+        'fields' => array('id'),
+        'conditions' => array(
+          'name' => 'Member'
+        ),
+        'contain' => false
+      ));
+
+      $this->data['User']['role_id'] = $role['Role']['id'];
+    }  
+
+    return true;
+
+  }
+
   public $belongsTo = array(
     'Role' => array(
       'className' => 'Role',
