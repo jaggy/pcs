bf19d5f9a1d45804929de2a080920190c3097ce6
Jaggy Gauran <jaggygauran@gmail.com>
2013-03-13 09:51:11 +0800

    added a basic view

--- Config/routes.php
+++ Config/routes.php
@@ -3,6 +3,7 @@
 	Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
 	Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
 
+  Router::connect('/users', array('controller' => 'users', 'action' => 'index')); 
   Router::connect('/register', array('controller' => 'users', 'action' => 'register')); 
   Router::connect('/activate/*', array('controller' => 'users', 'action' => 'activate'));
   
--- Controller/UsersController.php
+++ Controller/UsersController.php
@@ -3,6 +3,10 @@
 App::uses('AppController', 'Controller');
 class UsersController extends AppController{
 
+  public function index(){
+      
+  }
+
   /**
    * Registers the user
    * 
