2cf68696ace7ee6cce86bb80a319748f207f6b00
Jaggy Gauran <jaggygauran@gmail.com>
2013-03-18 03:30:15 +0800

    update the counter cache when the user joins a committee

--- Controller/CommitteesController.php
+++ Controller/CommitteesController.php
@@ -4,15 +4,35 @@ App::uses('AppController', 'Controller');
 class CommitteesController extends AppController{
 
 
+  public function pending(){
+      
+  }
+
   /**
    * Lets the user join a committee
    *   
    */
   public function join(){
+
+
     if($this->request->is('post')){
 
       $this->request->data['CommitteeUser']['user_id'] = $this->Session->read('Auth.User.id');
       if($this->Committee->CommitteeUser->save($this->request->data)){
+        $this->Committee->CommitteeUser->User->Behaviors->attach('Containable');
+
+        $user = $this->Committee->CommitteeUser->User->find('first', array(
+          'conditions' => array(
+            'User.id' => $this->Session->read('Auth.User.id')
+          ),
+          'contain' => array('Role', 'Committee')
+        ));
+        $session = $user['User'];
+
+        unset($user['User']);
+        $session = array_merge($session, $user);
+
+        $this->Auth->login($session);
         $this->Session->setFlash(__('Joined committee'));
         $this->redirect(array('action' => 'index'));
       }else{
