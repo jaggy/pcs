20cdff6f122981ad6fb6eaf2e3bceb997f64b17d
Jaggy Gauran <jaggygauran@gmail.com>
2013-03-18 02:25:22 +0800

    cannot rejoin a committee

--- Model/CommitteeUser.php
+++ Model/CommitteeUser.php
@@ -5,6 +5,29 @@ class CommitteeUser extends AppModel {
 
   public $useTable = 'committees_users';
 
+  public $validate = array(
+    'committee_id' => array(
+      'isRegistered'   => array(
+        'rule'    => array('isRegistered'),
+        'message' => 'You already signed up for this committee',
+        //'allowEmpty' => false,
+        //'required'   => false,
+        //'last'       => false, // Stop validation after this rule
+        //'on'         => 'create' // Limit validation to 'create' or 'update' operations
+      ),
+    )
+  );
+
+  public function isRegistered(){
+    $isRegistered = $this->find('count', array(
+      'conditions' => array(
+        'CommitteeUser.user_id' => $this->data['CommitteeUser']['user_id'],
+        'CommitteeUser.committee_id' => $this->data['CommitteeUser']['committee_id']
+      )
+    ));
+
+    return (!$isRegistered) ? true : false;
+  }
 
   public $belongsTo = array(
     'User' => array(
