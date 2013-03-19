98188914cb4a62eb9f989422f2453361d7798b4c
Jaggy Gauran <jaggygauran@gmail.com>
2013-03-20 00:11:36 +0800

    removed the global function

--- webroot/js/main.js
+++ webroot/js/main.js
@@ -1,9 +1,3 @@
 function last(array){
   return array[array.length-1];
 }
-
-function disappear(container){
-  container.animate({ height: 0, opacity: 0 }, 'slow', function(){
-    this.remove();
-  });
-}
