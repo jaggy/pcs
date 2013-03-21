c02e0649b0d0eca447a2f616b8ab17716bc0d0ed
Jaggy Gauran <jaggygauran@gmail.com>
2013-03-21 16:40:24 +0800

    fixed up after globalizing the anmation

--- webroot/js/committees/pending.js
+++ webroot/js/committees/pending.js
@@ -1,4 +1,6 @@
-$('a[class^="js-"]').on('click', function(){
+$('a[class^="js-"]').on('click', function(evt){
+  evt.preventDefault();
+
   var classes = $(this).attr('class').split(' ');
   var action = last(classes[0].split('-'));
   var committee = classes[1];
@@ -12,17 +14,19 @@ $('a[class^="js-"]').on('click', function(){
   var user_container = $(this).parent().parent();
   var committee_container = user_container.parent().parent();
 
-
-  if(committee_container.find('div.user').length === 1){
-    disappear(committee_container);
-  }
-
   $.post('/committees/pending.json', data, function(data){
     if(data.response){
-      disappear(user_container);
-      if($('div.committee').length === 0){
-        $('div.notice').fadeIn();
-      }
+      disappear(user_container, function(){
+
+        if(committee_container.find('div.user').length === 0){
+          committee_container.fadeOut('slow', function(){this.remove();});
+        }
+
+        if($('div.user').length === 0){
+          $('div.notice').fadeIn();
+        }
+      });
+
     }else{
       // print error message
     }
--- webroot/js/main.js
+++ webroot/js/main.js
@@ -2,13 +2,14 @@ function last(array){
   return array[array.length-1];
 }
 
-function appear(){
-
+function appear(contaner){
+  container.slideDown();
 }
 
-function disappear(container){
+function disappear(container, callback){
   container.animate({ height: 0, opacity: 0 }, 'slow', function(){
     this.remove();
 
+    if(callback !== undefined) setTimeout(callback, 500);
   });
 }
