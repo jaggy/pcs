f92380618fc623aabdfd5ff0c202a93ea49d3ddb
Jaggy Gauran <jaggygauran@gmail.com>
2013-03-29 13:03:30 +0800

    view files and actions

---
+++ Controller/AnnouncementsController.php
@@ -0,0 +1,16 @@
+<?php
+
+App::uses('AppController', 'Controller');
+class AnnouncementsController extends AppController{
+  
+    public function index(){}
+    
+    public function view($id = null){}
+    
+    public function add(){}
+
+    public function edit($id = null){}
+    
+    public function delete($id = null){}
+
+}
