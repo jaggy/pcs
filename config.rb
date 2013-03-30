5aaa5a4c39b95fad9bab335e1b23a3e708c46ed6
Jaggy Gauran <jaggygauran@gmail.com>
2013-03-30 09:17:29 +0800

    initialized sass

---
+++ config.rb
@@ -0,0 +1,23 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "webroot/css"
+sass_dir = "ebroot/sass"
+images_dir = "webroot/img"
+javascripts_dir = "webroot/js"
+
+output_style = :compressed
+environment = :production
+
+relative_assets = true
+
+line_comments = false
+color_output = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass View/Themed/Weatherph/webroot/sass scss && rm -rf sass && mv scss sass
