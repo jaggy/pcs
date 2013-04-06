445686c707123b9b635d4ca897d9b059555ecc46
Jaggy Gauran <jaggygauran@gmail.com>
2013-04-06 08:53:56 +0800

    fixed up saving of the event

--- Controller/EventsController.php
+++ Controller/EventsController.php
@@ -4,15 +4,29 @@ App::uses('AppController', 'Controller');
 class EventsController extends AppController{
   
   public function index(){ }
-  public function view($id = null){ }
+
+  public function view($id = null){
+
+    $this->Event->id = $id;
+    if(!$this->Event->exists()){
+      $this->redirect(array('action' => 'index'));
+    }
+
+
+  }
 
   public function add(){ 
 
     $this->Event->Committee->CommitteeUser->Behaviors->attach('Containable');
 
     if($this->request->is('post')){
-      debug($this->request->data);
-      exit;
+
+      if($this->Event->save($this->request->data)){
+        $this->Session->setFlash(__('Event created'));
+        $this->redirect(array('action' => 'index'));
+      }else{
+        $this->Session->setFlash(__('Uh-oh! Something went wrong'));
+      }
     }
 
     $committees = $this->Event->Committee->find('list');
--- View/Events/add.ctp
+++ View/Events/add.ctp
@@ -4,7 +4,7 @@
 <p><?php echo $this->Form->input('name'); ?></p>
 <p><?php echo $this->Form->input('location'); ?></p>
 <p><?php echo $this->Form->input('description'); ?></p>
-<p><label for="EventStartDate">Start Date</label><?php echo $this->Form->date('start_date'); ?></p>
+<p><label for="EventDate">Date</label><?php echo $this->Form->date('date'); ?></p>
 <p><?php echo $this->Form->input('all_day', array('label' => 'All Day Event', 'class' => 'js-toggle time')); ?></p>
 <p class="js-hide time">
   <label for="EventStartTime">Start Time</label><?php echo $this->Form->time('start_time'); ?>
