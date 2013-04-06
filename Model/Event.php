584ebad9b67d101529400efa952946a1ae006d05
Jaggy Gauran <jaggygauran@gmail.com>
2013-04-06 08:11:00 +0800

    added basic model validation

--- Model/Event.php
+++ Model/Event.php
@@ -3,6 +3,29 @@
 App::uses('AppModel', 'Model');
 class Event extends AppModel {
 
+  public $validate = array(
+    'name' => array(
+      'notempty'   => array(
+        'rule'    => array('notempty'),
+        'message' => 'name cannot be empty',
+        //'allowEmpty' => false,
+        //'required'   => false,
+        //'last'       => false, // Stop validation after this rule
+        //'on'         => 'create' // Limit validation to 'create' or 'update' operations
+      ),
+    ),
+    'location' => array(
+      'notempty'   => array(
+        'rule'    => array('notempty'),
+        'message' => 'location cannot be empty',
+        //'allowEmpty' => false,
+        //'required'   => false,
+        //'last'       => false, // Stop validation after this rule
+        //'on'         => 'create' // Limit validation to 'create' or 'update' operations
+      ),
+    )
+  );
+
     public $belongsTo = array(
       'User' => array(
         'className' => 'User',
